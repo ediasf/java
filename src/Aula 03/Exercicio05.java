c25e03349be2e1fee92168f49befbdda5698f19a
Edu <eduardo.diasf@mercadolivre.com>
2022-10-31 15:12:43 -0300

    Aula 03 Exercícios

---
+++ src/Aula 03/Exercicio05.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio05 {
+    public static Scanner Leia = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int numero,soma = 0, cont = 0;
+
+        do {
+            System.out.println("Digite um numero: ");
+            numero = Leia.nextInt();
+            if(numero > 0) {
+                soma = numero + soma;
+                cont++;
+            }
+        }while(numero > 0);
+        System.out.println("Contagem terminada, A soma dos números positivos é "+ soma);
+    }
+
+}
