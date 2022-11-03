820b8a68c3147a574dbd63d764c30e94920ec2d7
Eduardo Dias <115087594+ediasf@users.noreply.github.com>
2022-11-03 20:22:33 -0300

    Update Exercicio01.java
    backup pitch

--- src/Aula 01/Exercicio01.java
+++ src/Aula 01/Exercicio01.java
@@ -17,3 +17,39 @@ public class Exercicio01 {
 		System.out.println("Seu novo salário é de: " + novoSalario);
 	}
 }
+
+
+Bom dia pessoal, 
+
+Me chamo Eduardo Dias, tenho 25 anos, sou nascido e criado em Brasília, casado e pai da Arya de 1 aninho. 
+
+Sou apaixonado pelo universo de game of Thrones, por games e tecnologia. Meu hobby preferido é me reunir com amigos por voz ou pessoalmente para jogar e bater papo, além disso Me vejo como um profissional focado e determinado que não recusa desafios.
+.
+Visando aperfeiçoar minhas habilidades e currículo, busquei e busco fontes diversas de conhecimento.
+Dentre eles, 
+- Introdução a ciência da computação oferecida por Harvard, também
+- Estou no segundo período de Análise e Desenvolvimento de sistemas na Estácio.
+ E Atualmente, sou aluno no BootCamp de Back End com foco em Java e suas tecnologias oferecido pelo Mercado Livre em parceria com a Generation.
+.
+- Minha primeira experiência profissional se deu por volta de 2014, onde realizava montagem e manutenção dos computadores de algumas franquias do McDonalds de Brasília.
+
+  -	Por volta de 2016 foi quando iniciei um estágio no Sebrae Nacional, onde era responsável por dar suporte na extração de relatórios do banco de dados e na homologação de softwares.
+
+- Finalizando meu contrato, fui para um cargo de apurador de resultados na Ambev, onde era responsável por também extrair relatórios do banco de dados bem como elaborar planilhas, experiência essa que aprimorou muito minha capacidade analítica e gestão eficiente do tempo.
+
+Como podem ver, sempre estive conectado com a tecnologia de alguma forma, 
+e no início de 2022, meu principal objetivo era unir a liberdade de trabalhar do conforto da minha casa com um bom salário, 
+foi quando decidi voltar meu foco e energia para aprender programação e buscar uma oportunidade na área. 
+depois de alguns meses de estudo e persistência meu Sim chegou em dobro, na oportunidade de um estágio na IBM e na oportunidade de me tornar Júnior no mercado livre, apesar de não me sentir suficientemente preparado, abracei o desafio do Mercado Livre e me propus que entregaria o melhor de mim, afinal não há nada que eu não possa aprender e realizar com trabalho duro e persistência.
+
+Esse foi o meu pitch, espero que tenham gostado e se quiserem saber um pouco mais sobre a minha jornada vou deixar meus contatos aqui no chat, obrigado.
+
+
+
+
+
+
+
+
+
+
